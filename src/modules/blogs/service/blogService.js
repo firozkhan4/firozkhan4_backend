5a9e095de0bd55399bdeba3537ad26dd5c44b521
Firoz Khan <firozkhan907979@gmail.com>
2026-02-10 13:10:35 +0530

    fix: getAllBlog query sort error

--- src/modules/blogs/service/blogService.js
+++ src/modules/blogs/service/blogService.js
@@ -1,7 +1,7 @@
 import Blog from "../domain/Blog.js"
 
 async function getAllBlogs() {
-  const blogs = await Blog.find({ date: -1 });
+  const blogs = await Blog.find({}).sort({ date: -1 })
   return blogs
 }
 
