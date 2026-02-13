40e2db55ed2e3bd502bced933cf4db13f97b5f2e
Firoz Khan <firozkhan907979@gmail.com>
2026-02-13 10:18:32 +0530

    feat: rate limiter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from "express"
 import config from "./config.js"
 import connectDB from "./db.js"
 import cors from "cors"
-
+import { rateLimit } from 'express-rate-limit'
 
 import certificateRoutes from "./modules/certificates/route/certificateRoute.js"
 import blogRoutes from "./modules/blogs/route/blogRoute.js"
@@ -10,10 +10,20 @@ import blogRoutes from "./modules/blogs/route/blogRoute.js"
 const PORT = config.PORT
 const app = express()
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: 'draft-8',
+  legacyHeaders: false,
+  ipv6Subnet: 56,
+})
+
 
 app.use(cors({
   origin: config.ALLOW_URL.split(",")
 }))
+
+app.use(limiter)
 app.use(express.json())
 
 
